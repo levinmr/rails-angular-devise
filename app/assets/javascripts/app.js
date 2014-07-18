e6570bf7503f66b81c6ab8def0648a9843a35d56
Michael Levin <levinmr@upmc.edu>
2014-07-17 22:29:12 -0400

    Angular changes for production, add public/assets to .gitignore'

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,9 +10,13 @@ angular.module( 'myApp',
     'ngResource',
     'ngRoute',
   ]
-).config( function( $routeProvider ) {
+).config( ['$routeProvider', function( $routeProvider ) {
+  // Specifying the route provider as a string above ensures that the correct angular provider
+  // is passed to the function. Rails will shorten variable names as a part of minification
+  // in the production build, but not strings
+
   $routeProvider.when( '/',      { templateUrl: 'main.html',  controller: 'MainCtrl' } );
   $routeProvider.when( '/about', { templateUrl: 'about.html', controller: 'AboutCtrl' } );
 
   $routeProvider.otherwise( { redirectTo: '/' } );
-} );
+} ] );
